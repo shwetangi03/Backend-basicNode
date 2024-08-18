ee53f1cdd727a86378698b226058b4ac453df520
Shwetangi <shwetangichaurasia4@gmail.com>
2024-08-18 23:42:48 +0530

    Request / Response

--- first-program/index.js
+++ first-program/index.js
@@ -5,6 +5,20 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
+app.get("/home", function (req, res) {
+  res.send("Welcome home");
+});
+
+app.get("/about", function (req, res) {
+  res.send("Welcome to About Us page");
+});
+
+app.get("/node", function (req, res) {
+  res.send("Welcome to my Node Js project");
+});
+
 app.listen(4000, function () {
   console.log("Server is running on port 4000");
 });
+
+
